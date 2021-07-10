320ed1448808442b1b6d93e7d58cc51436e96bd5
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-07-11 02:30:31 +0530

    Export state for isLoadingMore

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -15,6 +15,7 @@ export const useHomeFetch = () => {
     const [state, setState] = useState(initialState);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [isLoadingMore, setIsLoadingMore] = useState(false);
 
     const fetchMovies = async (page, searchTerm = "") => {
         try {
@@ -42,5 +43,12 @@ export const useHomeFetch = () => {
         fetchMovies(1, searchTerm);
     }, [searchTerm]);
 
-    return { state, loading, error, searchTerm, setSearchTerm };
+    return {
+        state,
+        loading,
+        error,
+        searchTerm,
+        setSearchTerm,
+        setIsLoadingMore,
+    };
 };
