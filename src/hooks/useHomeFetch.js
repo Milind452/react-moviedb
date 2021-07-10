8dca8077f4676f1fe40243325e685f0ff8bb3149
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-07-11 02:05:24 +0530

    Export searchTerm

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -42,5 +42,5 @@ export const useHomeFetch = () => {
         fetchMovies(1, searchTerm);
     }, [searchTerm]);
 
-    return { state, loading, error, setSearchTerm };
+    return { state, loading, error, searchTerm, setSearchTerm };
 };
