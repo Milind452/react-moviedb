f7ecf3a204625422564646e7e963be1b0e4fd8ef
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-07-12 02:23:52 +0530

    Fix Actor to return implicitly

--- src/components/Actor/Actor.js
+++ src/components/Actor/Actor.js
@@ -3,12 +3,12 @@ import React from "react";
 //Styles
 import { Wrapper, Image } from "./Actor.styles";
 
-const Actor = ({ name, character, imageUrl }) => {
+const Actor = ({ name, character, imageUrl }) => (
     <Wrapper>
         <Image src={imageUrl} alt="actor-thumbnail" />
         <h3>{name}</h3>
         <p>{character}</p>
-    </Wrapper>;
-};
+    </Wrapper>
+);
 
 export default Actor;
