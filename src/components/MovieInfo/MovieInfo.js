0c13acec78637e97feaedd85d8918d85663ba767
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-07-11 20:17:58 +0530

    Add text to MovieInfo

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -24,6 +24,24 @@ const MovieInfo = ({ movie }) => (
                 clickable={false}
                 alt="movie-thumbnail"
             />
+            <Text>
+                <h1>{movie.title}</h1>
+                <h3>PLOT</h3>
+                <p>{movie.overview}</p>
+
+                <div className="rating-directors">
+                    <div>
+                        <h3>Rating</h3>
+                        <div className="score">{movie.vote_average}</div>
+                    </div>
+                    <div className="director">
+                        <h3>DIRECTOR{movie.directors.length > 1 ? "S" : ""}</h3>
+                        {movie.directors.map((director) => (
+                            <p key={director.credit_id}>{director.name}</p>
+                        ))}
+                    </div>
+                </div>
+            </Text>
         </Content>
     </Wrapper>
 );
