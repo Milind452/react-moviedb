bbba01cf7f6e4f13edff112cab8c65885ba1abbe
Milind Mohapatra <mohapatra.milind@gmail.com>
2021-07-12 02:23:36 +0530

    Add Actors to Movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -10,6 +10,7 @@ import Spinner from "./Spinner/Spinner";
 import BreadCrumb from "./Breadcrumb/BreadCrumb";
 import MovieInfo from "./MovieInfo/MovieInfo";
 import MovieInfoBar from "./MovieInfoBar/MovieInfoBar";
+import Actor from "./Actor/Actor";
 
 //Hooks
 import { useMovieFetch } from "../hooks/useMovieFetch";
@@ -33,6 +34,20 @@ const Movie = () => {
                 budget={movie.budget}
                 revenue={movie.revenue}
             />
+            <Grid header="Actors">
+                {movie.actors.map((actor) => (
+                    <Actor
+                        key={actor.credit_id}
+                        name={actor.name}
+                        character={actor.character}
+                        imageUrl={
+                            actor.profile_path
+                                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                                : NoImage
+                        }
+                    />
+                ))}
+            </Grid>
         </>
     );
 };
